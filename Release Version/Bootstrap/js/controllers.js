44fdde4a539bf4bf746c7615d88c79362378f59d
Matt Allen <Mallen-cc@conestogac.on.ca>
2016-07-11 16:37:24 -0400

    Products works now

--- Release Version/Bootstrap/js/controllers.js
+++ Release Version/Bootstrap/js/controllers.js
@@ -1,5 +1,5 @@
-angular.module('anchorScroll', [])
-.run(['$anchorScroll', function($anchorScroll)
+var app = angular.module('pharmacyApp', []);
+app.run(['$anchorScroll', function($anchorScroll)
 {
 	$anchorScroll.yOffset = 50;
 }])
@@ -21,10 +21,14 @@ angular.module('anchorScroll', [])
 	}
 ]);
 
-app.controller("ProductsCtrl", function($scope)
-{
-	$scope.products = ['Compression Therapy', 'Diagnostics / Diabetic Supplies',
-	'Mobility Aids', 'Incontinent / Ostomy Supplies', 'Orthopedic Supports', 
-	'Bath Safety', 'Daily Living Aids'];
-	$scope.productShow = '';
+app.controller('ProductsCtrl', function($scope)
+{	
+	$scope.products = [
+	{id: 1, name: "Compression Therapy"},
+	{id: 2, name: "Diagnostics / Diabetic Supplies"},
+	{id: 3, name: "Mobility Aids"},
+	{id: 4, name: "Incontinent / Ostomy Supplies"},
+	{id: 5, name: "Orthopedic Supports"},
+	{id: 6, name: "Bath Safety"},
+	{id: 7, name: "Daily Living Aids"}];
 });
