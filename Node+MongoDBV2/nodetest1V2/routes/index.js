1f5845a329afe534a93704e42a904ad99e25189f
sgokhe <Sgokhe-cc@conestogac.on.ca>
2016-07-28 17:06:12 -0400

    Version 2 uploaded
    Need fixing of new field

---
+++ Node+MongoDBV2/nodetest1V2/routes/index.js
@@ -0,0 +1,105 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+/* GET Employeelist page. */
+router.get('/employeelist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('employeecollection');
+    collection.find({},{},function(e,docs){
+        res.render('employeelist', {
+            "employeelist" : docs
+        });
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+/* GET New Employee page. */
+router.get('/newemployee', function(req, res) {
+    res.render('newemployee', { title: 'Add New employee' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
+/* POST to Add Employee Service */
+router.post('/addemployee', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+	var employeeId = req.body.employeeid;
+    var employeeName = req.body.employeename;
+    var employeeEmail = req.body.employeeemail;
+
+    // Set our collection
+    var collection = db.get('employeecollection');
+
+    // Submit to the DB
+    collection.insert({
+		"employeeid" : employeeId,
+        "employeename" : employeeName,
+        "employeemail" : employeeEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("employeelist");
+        }
+    });
+});
+module.exports = router;
